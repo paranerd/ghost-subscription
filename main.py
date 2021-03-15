4c339accca6b290993799553cc9abda1267e0eb4
paranerd <paranerd.development@gmail.com>
2021-03-15 13:54:17 +0100

    Added HTML mail templates

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
-import os
+import hashlib
 import requests
 import lxml.html
-import hashlib
-from utils import util
+import jinja2
 from utils.config import ConfigHelper
 from utils.cache import Cache
 from utils.sites import Sites
 from utils import mail
 
+TEMPLATE_FILE = "templates/base.html"
+
 config = ConfigHelper()
 cache = Cache()
 sites = Sites()
 
+
 def run():
     subs = config.get('subscriptions')
+    updates = []
 
     for sub in subs:
-        check(sub, sites.get(sub))
+        update = check(sub, sites.get(sub))
+
+        if update:
+            updates.append(update)
+
+    html = render_html(updates)
+
+    if len(updates) > 0:
+        send(html)
+
 
 def check(sub, site):
     res = requests.get(site['url'])
@@ -30,18 +42,31 @@ def check(sub, site):
     if cached != hashed:
         link = root.xpath(site['link'])[0]
 
-        send(title, link)
         update_cache(sub, hashed)
 
+        return {
+            'name': sub,
+            'title': title.strip(),
+            'link': link
+        }
+
+
 def update_cache(name, value):
-    print('Setting {} to {}'.format(name, value))
     cache.set(name, value)
 
-def send(title, link):
-    print('Sending:')
-    print('Title: {}'.format(title.strip()))
-    print('Link: {}'.format(link))
-    mail.send_gmail(config.get('mail_user'), config.get('mail_pass'), config.get('mail_user'), 'Ghost Subscription', 'worx')
+
+def render_html(updates):
+    template_loader = jinja2.FileSystemLoader(searchpath="./")
+    template_env = jinja2.Environment(loader=template_loader, autoescape=True)
+    template = template_env.get_template(TEMPLATE_FILE)
+
+    return template.render(updates=updates)
+
+
+def send(html):
+    mail.send_gmail(config.get('mail_user'), config.get('mail_pass'),
+                    [config.get('mail_user')], 'Ghost Subscription', html)
+
 
 if __name__ == '__main__':
     run()
